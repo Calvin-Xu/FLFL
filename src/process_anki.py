215d5af1348061d4a4043ae971fa95e31705b721
Pinlin [Calvin] Xu <calvinxu806@gmail.com>
2024-07-31 17:09:00 -0700

    Improve furigana handling

--- src/process_anki.py
+++ src/process_anki.py
@@ -19,11 +19,17 @@ def extract_entries(file_path):
     return examples
 
 
-def generate_kanji_reading_pairs(lemma, reading):
-    # ! it is impossible to determine this from the arguments alone
+def generate_possible_kanji_reading_pairs(text, reading):
+    # ! it is impossible to determine an unique reading from the arguments alone
     # for example, 持ち力 can be (も)ち（ちから）or（もち）ち（から） if you don't know anything
     # this function generates all valid readings (fully-aligned, each kanji mapped to at least one kana)
-    # additionally, short readings of kanji are prioritized
+    # and greedily short readings of kanji are at the front of the list (and can be used)
+
+    # this is a good heuristic for short text (like a MeCab token)
+    # but fails for for example
+    # 鹿乃子のこのこ虎視眈々, しかのこのこのここしたんたん
+    # 鹿乃子(しか)のこのこ虎視眈々(のここしたんたん)
+
     def is_kana(char):
         return is_hiragana(char) or is_katakana(char)
 
@@ -31,9 +37,9 @@ def generate_kanji_reading_pairs(lemma, reading):
         raise ValueError("generate_furigana:reading must be in kana")
     # recursive
     # base cases
-    if all(is_kanji(char) for char in lemma):
-        return [(lemma, reading)]
-    if is_kana(lemma[-1]) and lemma[-1] != reading[-1]:
+    if all(is_kanji(char) for char in text) or all(is_kana(char) for char in text):
+        return [[(text, reading)]]
+    if is_kana(text[-1]) and text[-1] != reading[-1]:
         return None
 
     class States(Enum):
@@ -47,54 +53,56 @@ def generate_kanji_reading_pairs(lemma, reading):
 
     results = []
 
-    while True:
-        # print(lemma, reading, state)
+    while text and reading:
+        print(text, reading, state)
         match state:
             case States.START:
-                if is_kana(lemma[-1]):
+                if is_kana(text[-1]):
                     state = States.KANA
-                    lemma = lemma[:-1]
+                    text = text[:-1]
                     reading = reading[:-1]
                 else:
                     state = States.KANJI
             case States.KANA:
-                if is_kanji(lemma[-1]):
+                if is_kanji(text[-1]):
                     state = States.KANJI
                 else:
-                    lemma = lemma[:-1]
+                    text = text[:-1]
                     reading = reading[:-1]
                     state = States.KANA
             case States.KANJI:
-                if all(is_kanji(char) for char in lemma):
-                    return [(lemma, reading)]
-                current_kanji_block = lemma[-1] + current_kanji_block
-                if is_kana(lemma[-2]):
-                    splits = []
-                    # note that this ordering means short readings are in the front of results
-                    # which is a good heuristic
-                    for j in range(len(reading)):
-                        if reading[j] == lemma[-2]:
-                            split = (reading[: j + 1], reading[j + 1 :])
-                            splits.append(split)
-                    # print(splits)
-                    results = [
-                        (
-                            generate_kanji_reading_pairs(lemma[:-1], split[0]),
-                            (current_kanji_block, split[1]),
-                        )
-                        for split in splits
-                    ]
+                if all(is_kanji(char) for char in text):
+                    results = [[(text, reading)]]
                     state = States.END
                 else:
-                    state = States.KANJI
+                    current_kanji_block = text[-1] + current_kanji_block
+                    if is_kana(text[-2]):
+                        splits = []
+                        # shortest readings are in the front of results
+                        for j, char in enumerate(reading):
+                            if char == text[-2]:
+                                split = (reading[: j + 1], reading[j + 1 :])
+                                splits.append(split)
+                        # print(splits)
+                        results = [
+                            result + [(current_kanji_block, split[1])]
+                            for split in splits
+                            for result in generate_possible_kanji_reading_pairs(
+                                text[:-1], split[0]
+                            )
+                            if result is not None
+                        ]
+                        # print(results)
+                        state = States.END
+                    else:
+                        state = States.KANJI
+                        text = text[:-1]
             case States.END:
-                # print(results)
-                for result in results:
-                    if result[0] is not None:
-                        return result[0] + [result[1]]
+                break
+    return results
 
 
-def generate_furigana(lemma, reading, delimiters):
+def generate_furigana(text, reading, delimiters):
     def replace_first(text, lemma, reading):
         index = text.find(lemma)
         if index != -1:
@@ -104,25 +112,38 @@ def generate_furigana(lemma, reading, delimiters):
             return before + ruby_text, after
         return text, ""
 
-    text = ("", lemma)
-    pairs = generate_kanji_reading_pairs(lemma, reading)
+    _text = ("", text)
+    pairs = generate_possible_kanji_reading_pairs(text, reading)[0]
     for pair in pairs:
-        left, right = replace_first(text[1], pair[0], pair[1])
-        text = (text[0] + left, right)
-    return "".join(text)
+        lemma, reading = pair
+        left, right = replace_first(_text[1], lemma, reading)
+        _text = (_text[0] + left, right)
+    return "".join(_text)
+
+
+def test_furigana():
+    tests = [
+        # ("持ち力と届かない", "もちちからととどかない"),
+        # ("持ち越し", "もちこし"),
+        # ("子", "こ"),
+        # ("朽ちる", "くちる"),
+        # ("房々", "ふさふさ"),
+        # ("蛮殻", "バンカラ"),
+        # ("がぶ飲み", "がぶのみ"),
+        # ("已んぬる哉", "やんぬるかな"),
+        # ("付きっ切り", "つきっきり"),
+        # ("歯が痛いので歯科医に診てもらった", "はがいたいのでしかいにみてもらった"),
+        ("鹿乃子のこのこ虎視眈々", "しかのこのこのここしたんたん")
+    ]
+    delimiters = {"ruby": ("<ruby>", "</ruby>"), "rt": ("<rt>", "</rt>")}
+    for test in tests:
+        print(generate_furigana(test[0], test[1], delimiters))
 
 
 def main():
     # examples = extract_entries("anki_dataset/Mining-All-1.txt")
     # print(examples)
-    lemma = "持ち力と届かない"
-    reading = "もちちからととどかない"
-    # lemma = "持ち越し"
-    # reading = "もちこし"
-    # lemma = "子"
-    # reading = "こ"
-    delimiters = {"ruby": ("<ruby>", "</ruby>"), "rt": ("<rt>", "</rt>")}
-    print(generate_furigana(lemma, reading, delimiters))
+    test_furigana()
 
 
 if __name__ == "__main__":
